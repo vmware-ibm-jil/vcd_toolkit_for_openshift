11ae58b9502b07e75effbd7f04fe8a9564f82a8c
Stuart D Lipshires <stu_lipshires@us.ibm.com>
2020-12-11 10:12:16 -0500

    updated for cp4m defs

--- src/vcd-cp4m.sh
+++ src/vcd-cp4m.sh
@@ -25,7 +25,7 @@ done
 
 
 
-for i in `cat $VMLIST1` 
+for i in `cat $VMLIST1`
   do
   grep $i $VMLIST|grep bootstrap > /dev/null 2>&1
   if [ $? = 0 ]
@@ -111,6 +111,40 @@ mv $IGNITIONDIR/custom_property.xml $IGNITIONDIR/worker-0.custom_property.xml
   mv $IGNITIONDIR/custom_property.xml $IGNITIONDIR/worker-2.custom_property.xml
       fi
 
+      grep $i $VMLIST|grep storage-0 > /dev/null 2>&1
+      if [ $? = 0 ]
+        then
+        echo storage-0 FOUND
+        cp $OPENSHIFT/custom_property.xml $IGNITIONDIR/custom_property.xml
+        a=`cat $IGNITIONDIR/$STORAGENAME-00.64`
+        sed -i "s/###DATA###/$a/" $IGNITIONDIR/custom_property.xml
+        curl -i -k -H "Accept: application/*+xml;version=31.0" -H "x-vcloud-authorization: $auth" -H "Content-Type: application/vnd.vmware.vcloud.productSections+xml" -X PUT  $i/productSections -d "@$IGNITIONDIR/custom_property.xml"
+        echo $i
+    mv $IGNITIONDIR/custom_property.xml $IGNITIONDIR/storage-0.custom_property.xml
+        fi
+
+        grep $i $VMLIST|grep storage-1 > /dev/null 2>&1
+        if [ $? = 0 ]
+          then
+          echo storage-1 FOUND
+          cp $OPENSHIFT/custom_property.xml $IGNITIONDIR/custom_property.xml
+          a=`cat $IGNITIONDIR/$STORAGENAME-01.64`
+          sed -i "s/###DATA###/$a/" $IGNITIONDIR/custom_property.xml
+          curl -i -k -H "Accept: application/*+xml;version=31.0" -H "x-vcloud-authorization: $auth" -H "Content-Type: application/vnd.vmware.vcloud.productSections+xml" -X PUT  $i/productSections -d "@$IGNITIONDIR/custom_property.xml"
+          echo $i
+      mv $IGNITIONDIR/custom_property.xml $IGNITIONDIR/storage-1.custom_property.xml
+
+      grep $i $VMLIST|grep storage-2 > /dev/null 2>&1
+      if [ $? = 0 ]
+        then
+        echo storage-2 FOUND
+        cp $OPENSHIFT/custom_property.xml $IGNITIONDIR/custom_property.xml
+        a=`cat $IGNITIONDIR/$STORAGENAME-02.64`
+        sed -i "s/###DATA###/$a/" $IGNITIONDIR/custom_property.xml
+        curl -i -k -H "Accept: application/*+xml;version=31.0" -H "x-vcloud-authorization: $auth" -H "Content-Type: application/vnd.vmware.vcloud.productSections+xml" -X PUT  $i/productSections -d "@$IGNITIONDIR/custom_property.xml"
+        echo $i
+    mv $IGNITIONDIR/custom_property.xml $IGNITIONDIR/storage-2.custom_property.xml
+        fi
   grep $i $VMLIST|grep -i loadbalancer > /dev/null 2>&1
   if [ $? = 0 ]
     then
@@ -128,6 +162,9 @@ mv $IGNITIONDIR/custom_property.xml $IGNITIONDIR/worker-0.custom_property.xml
     sed -i "s/###WORKER0FQDN###/$WORKERNAME-00.$DOMAIN/" $IGNITIONDIR/custom_property.xml
     sed -i "s/###WORKER1FQDN###/$WORKERNAME-01.$DOMAIN/" $IGNITIONDIR/custom_property.xml
     sed -i "s/###WORKER2FQDN###/$WORKERNAME-02.$DOMAIN/" $IGNITIONDIR/custom_property.xml
+    sed -i "s/###STORAGE0FQDN###/$STORAGENAME-00.$DOMAIN/" $IGNITIONDIR/custom_property.xml
+    sed -i "s/###STORAGE1FQDN###/$STORAGENAME-01.$DOMAIN/" $IGNITIONDIR/custom_property.xml
+    sed -i "s/###STORAGE2FQDN###/$STORAGENAME-02.$DOMAIN/" $IGNITIONDIR/custom_property.xml
     sed -i "s/###IPADDRESS###/$LB/" $IGNITIONDIR/custom_property.xml
     sed -i "s/###NETMASK###/$NETMASK/" $IGNITIONDIR/custom_property.xml
     sed -i "s/###DNS###/$PUBLICDNS/" $IGNITIONDIR/custom_property.xml
@@ -143,4 +180,3 @@ mv $IGNITIONDIR/custom_property.xml $IGNITIONDIR/worker-0.custom_property.xml
 mv $IGNITIONDIR/custom_property.xml $IGNITIONDIR/loadbalancer.custom_property.xml
   fi
 done
-
